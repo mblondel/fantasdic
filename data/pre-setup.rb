828c9efa94c8286825b004804f37579a6bac124a
Mathieu Blondel <mblondel@src.gnome.org>
2007-10-19 16:11:54 +0000

    Don't copy .svn dirs.
    * data/pre-setup.rb: Don't copy .svn dirs.
    
    svn path=/trunk/; revision=229

--- data/pre-setup.rb
+++ data/pre-setup.rb
@@ -2,8 +2,19 @@
 
 # Copying documentation figures
 require 'fileutils'
+
 FileUtils.mkdir_p('doc/fantasdic/html/')
-FileUtils.cp_r(Dir.glob('gnome/help/fantasdic/*'), 'doc/fantasdic/html/')
+
+Dir.glob("gnome/help/fantasdic/**/*").each do |file_to_copy|
+    file = file_to_copy.gsub("gnome/help/fantasdic/", "")
+    file = File.join("doc/fantasdic/html/", file)
+    if File.directory? file_to_copy
+        FileUtils.mkdir_p(file)
+    else
+        FileUtils.cp(file_to_copy, file)
+    end
+end
+
 Dir.new("doc/fantasdic/html").each do |dir|
     next if [".", ".."].include? dir
     dir = File.join("doc/fantasdic/html", dir)
@@ -12,3 +23,4 @@ Dir.new("doc/fantasdic/html").each do |dir|
         FileUtils.rm_f(Dir.glob(File.join(dir, "*.mo")))
     end
 end
+
